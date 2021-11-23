7a73213feaaf8dd9cb606acfac2a73a63d78c9d1
Eduardo Gualberto <58743989+eduardo-gualberto@users.noreply.github.com>
2021-11-23 13:40:27 -0300

    Delete src/agenda_telefonica directory

--- src/agenda_telefonica/Abstract.java
+++
@@ -1,5 +0,0 @@
-package agenda_telefonica;
-
-public class Abstract {
-    public static final String DATABASE_URL = "jdbc:postgresql://localhost/Agenda_telefonica?user=postgres&password=postegres";
-}
